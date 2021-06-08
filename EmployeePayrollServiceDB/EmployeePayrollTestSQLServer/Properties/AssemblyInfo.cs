137d86a4bc287dec3d0ae368b44bd002e6c68d10
Rohit1823 <rohitmachale1823@gmail.com>
2021-06-08 18:14:58 +0530

    [Rohit]ADD: Employee payroll service using ADO.NET Project File

---
+++ EmployeePayrollServiceDB/EmployeePayrollTestSQLServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeePayrollTestSQLServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("EmployeePayrollTestSQLServer")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b97391e-f7eb-4795-b1f0-1e20af77c3bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
